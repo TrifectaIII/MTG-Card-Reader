bde4071de387fca5cc23f15c73798e5292045416
Dakota Madden-Fong <dmaddenf@willamette.edu>
2019-04-04 08:51:04 -0700

    Add specific version of opencv-contrib to install_packages script

--- install_packages.py
+++ install_packages.py
@@ -5,7 +5,7 @@ import subprocess
 def install(name):
     subprocess.call(['pip', 'install', name])
     
-pkgs = ['opencv-contrib-python','PyQt5']
+pkgs = ['opencv-contrib-python==3.4.2.17','PyQt5']
 
 for pkg in pkgs:
     install(pkg)
